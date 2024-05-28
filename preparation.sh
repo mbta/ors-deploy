6aa80359a9d218845e2c33155d0ee72979e4291d
Paul Swartz <paul@paulswartz.net>
2024-05-28 10:52:58 -0400

    fix: stop the loop if the server crashes

--- preparation.sh
+++ preparation.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
 export REBUILD_GRAPHS=true
-export ors.engine.preparation_mode=true
-export ors.services.routing.mode=preparation
+export ORS_ENGINE_PREPARATION_MODE=true
+export ORS_SERVICES_ROUTING_MODE=preparation
 
 /entrypoint.sh &
 entrypoint_pid=$!
 
-# Wait for the server to become healthy...
-while ! wget --quiet -O /dev/null http://localhost:8082/ors/v2/health; do
+# Wait for the server to stop or become healthy...
+while kill -0 $entrypoint_pid > /dev/null && ! wget --quiet -O /dev/null http://localhost:8082/ors/v2/health; do
     sleep 5
 done
 
 echo Built graph, stopping server...
-kill $entrypoint_pid
+kill $entrypoint_pid || true
+
+echo Checking that graphs were built...
+test -d graphs/car || exit 1
+test -d graphs/hgv || exit 1
