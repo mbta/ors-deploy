916d2be6cb2e2920fa5718485a8529d2c64a42e8
Brian Fauble <bfauble@users.noreply.github.com>
2025-05-21 10:25:12 -0600

    use o5m file type and fix shellcheck warnings

--- changesets/scripts/entrypoint.sh
+++ changesets/scripts/entrypoint.sh
@@ -11,32 +11,21 @@ else
   echo "Downloading osm data"
   wget http://download.geofabrik.de/north-america/us/massachusetts-latest.osm.pbf -O files/data.osm.pbf
 
-  echo "Converting osm.pbf file to osm format"
-  osmconvert files/data.osm.pbf > files/data.osm
+  echo "Converting osm.pbf file to o5m format"
+  osmconvert files/data.osm.pbf > files/data.o5m
 fi
 
 printf "Filtering OSM data for ways with %s\n" "$search_key=$search_value"
-osmfilter files/data.osm --ignore-dependencies --keep= --keep-ways="hgv=no and $search_key=$search_value" > tmp/filtered.osm
+osmfilter files/data.o5m --ignore-dependencies --keep= --keep-ways="hgv=no and $search_key=$search_value" > tmp/filtered.o5m
 
 printf "Modifying filtered OSM tags with %s\n" "$modify_string"
-osmfilter tmp/filtered.osm --modify-tags="$modify_string" > tmp/modified.osm
-
-printf "Updating version number of modified ways\n"
-while IFS= read -r line; do
-  if echo "$line" | grep -q "<way"; then
-    version=$(echo "$line" | grep -o 'version="[0-9]*"' | grep -o '[0-9]*')
-    if [ -n "$version" ]; then
-      new_version=$((version + 1))
-      line=$(echo "$line" | sed "s/version=\"$version\"/version=\"$new_version\"/")
-    fi
-  fi
-  echo "$line"
-done < tmp/modified.osm > tmp/version_updated.osm
-
+osmfilter tmp/filtered.o5m --modify-tags="$modify_string" --drop-version > tmp/modified.o5m
 
 timestamp=$(date +%s)
 changeset_name="$timestamp"_changeset.osc
 
 printf "Creating changeset file: %s\n" "$changeset_name"
 
-osmconvert tmp/filtered.osm tmp/version_updated.osm --diff -o="$changeset_name"
+osmconvert tmp/filtered.o5m tmp/modified.o5m --diff -o="$changeset_name"
+
+rm -rf tmp/
